5a483793f63da27260e267b56f5e1959b1a75f36
Aleksandr Potekhin <55990036+rendleks@users.noreply.github.com>
2026-02-09 00:49:57 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -25,7 +25,6 @@ public class App {
 
         switch (selectedGame) {
             case "0":
-                System.exit(0);
                 break;
             case "1":
                 Cli.greet();
