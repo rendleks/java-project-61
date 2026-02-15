6ed8afd4baf1622789bd39bce30dfe4b19de98bc
rendleks <0rendl050@gmail.com>
2026-02-15 21:33:01 +0300

    update engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -44,10 +44,6 @@ public class Engine {
         if (countCorrectAnswer == 3) {
             System.out.println("Congratulations, " + userName + "!");
         }
-<<<<<<< HEAD
-=======
-        
->>>>>>> feafb35e751e2ac9571ff7e28187532dc847c244
     }
 
 }
