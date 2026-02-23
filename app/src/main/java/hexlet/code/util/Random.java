6ec1c8ff952fc4c782063d93970f1ad3be2ff715
rendleks <0rendl050@gmail.com>
2026-02-23 12:55:59 +0300

    refactor util.random

--- app/src/main/java/hexlet/code/util/Random.java
+++ app/src/main/java/hexlet/code/util/Random.java
@@ -1,19 +1,9 @@
 package hexlet.code.util;
 
 public class Random {
-    private static final int MAX_NUMBER = 100;
-    private static final int MIN_NUMBER = 1;
 
     public static int generateNumber(int min, int max) {
         return (int) Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    public static int generateNumber(int max) {
-        return (int) Math.floor(Math.random() * (max - MIN_NUMBER + 1)) + MIN_NUMBER;
-    }
-
-    public static int generateNumber() {
-        return generateNumber(MIN_NUMBER, MAX_NUMBER);
     }
 
 }
